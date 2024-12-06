c0f146a07dbba6119bacb6d99b2c042e2a2928c3
IdriH <114089795+IdriH@users.noreply.github.com>
2024-12-06 12:20:32 +0100

    spring web service templt

---
+++ src/main/java/it/alfasoft/myPersonalDriver/SpringPDaccountsRS/SpringPDaccountsRsApplication.java
@@ -0,0 +1,13 @@
+package it.alfasoft.myPersonalDriver.SpringPDaccountsRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPDaccountsRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPDaccountsRsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/alfasoft/myPersonalDriver/SpringPDaccountsRS/SpringPDaccountsRsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.alfasoft.myPersonalDriver.SpringPDaccountsRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPDaccountsRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
